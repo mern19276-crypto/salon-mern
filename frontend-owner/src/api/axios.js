4feb7ad849ac6d308c728a7fdff668bf10435c73
heshan2002 <it22107664@my.sliit.lk>
2025-08-22 10:49:46 +0530

    a

--- frontend-owner/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend shared base URL
-  withCredentials: true,
-});
-
-export default instance;
